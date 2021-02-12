8efb24cb33aeda9b32c4636b620c9b2d124eaf77
Alejandro Cuello <acuellodemoya@gmail.com>
2021-02-12 15:14:43 -0500

    12022021

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+
+var args = {"width":300,"Height":300,"duration":5000,"transition":2000,"effect":"fade","id":"divGallery",
+            "images":[["img/c1.jpg","Construccion de sitios web con Wordpress"],
+                        ["img/c2.jpg","HTML y CSS3"],
+                        ["img/c3.jpg", "Machine learning course"],
+                        ["img/c4.jpg","JavaScript Tutorial"],
+                        ["img/c5.JPG","POO y algoritmos con Python"],
+                        ["img/c6.JPG", "Introduccion al machine learning"],
+                        ["img/c7.JPG", "Curso de Python"],
+                        ["img/c8.JPG", "Git y Github"],
+                        ["img/c9.JPG", "Introduccion a la Terminal y linea de Comandos"],
+                        ["img/c10.JPG", "Fundamentos de Bases de Datos"],
+                        ["img/c11.JPG", "PHP con laravel"],
+                        ["img/c12.JPG", "Programacion orientada aobjetos"],
+            ]}
+slideGallery.gallery(args);
